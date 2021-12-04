d232b06b1cf4ecbe201b2c82efcee41094cf632e
unknown <sudipt0102@gmail.com>
2021-12-04 18:19:03 +0530

    2 and 3 are added

---
+++ 2.CreatingObject.py
@@ -0,0 +1,6 @@
+class Item:
+    pass
+
+# here item is created object of the class
+item = Item()
+
---
+++ 3.ClassAttributes.py
@@ -0,0 +1,16 @@
+class Item:
+    pass
+
+item1 = Item()
+# these are class varible because it belongs to the object (instances of this class)
+# these are assigned attributes of this instance of class Item
+# Note: coming from java, it can be done outside of the class. 
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5 
+
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3 
+
